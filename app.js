613b952d6c11f4d7d5a50ac83b82c63b89588367
ASL-PrimeWin <omgplayz150@gmail.com>
2026-09-05 16:27:03 +0530

    Delete app.js

--- app.js
+++
@@ -1,2 +0,0 @@
-function go(){const q=document.getElementById('search').value.trim();if(q)location.href='colleges.html?q='+encodeURIComponent(q);}
-function quick(q){document.getElementById('search').value=q;go();}
